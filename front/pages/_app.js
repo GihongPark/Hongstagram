3090a80e5249e5534c91ec0b199c25323c34de64
gipark <gipark1993@gmail.com>
2020-08-07 23:06:23 +0900

    init front next, redux-saga 적용

---
+++ front/pages/_app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import 'antd/dist/antd.css';
+
+import wrapper from '../store/configureStore';
+
+const App = ({ Component }) => (
+  <>
+    <Head>
+      <title>Hongstagram</title>
+    </Head>
+    <Component />
+  </>
+);
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+};
+
+export default wrapper.withRedux(App);
---
+++ front/reducers/index.js
@@ -0,0 +1,20 @@
+import { HYDRATE } from 'next-redux-wrapper';
+import { combineReducers } from 'redux';
+
+import user from './user';
+
+const rootReducer = (state, action) => {
+  switch (action.type) {
+    case HYDRATE: // next에서 페이지 열때 getServerSideProps 정보를 담아줌
+      console.log('HYDRAYE', action);
+      return action.payload;
+    default: {
+      const combinedReducer = combineReducers({
+        user,
+      });
+      return combinedReducer(state, action);
+    }
+  }
+};
+
+export default rootReducer;
