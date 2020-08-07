c9909abcfe798e94b3c2542361fe425845c964f9
gipark <gipark1993@gmail.com>
2020-08-07 22:12:47 +0900

    add postAPI 게시물 업로드 이미지 업로드

--- back/routes/post.js
+++ back/routes/post.js
@@ -1,15 +1,85 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 
-const { User, Post, Image, Comment } = require('../models');
+const { User, Post, Image, Comment, Hashtag } = require('../models');
+const { isLoggedIn } = require('./middlewares');
+
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {  // GET /post
-  try {
+// uploads 폴더 생성
+try{
+  fs.accessSync('uploads');
+} catch (error) {
+  console.log('uploads 폴더가 없으므로 생성합니다.');
+  fs.mkdirSync('uploads');
+}
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, 'uploads');
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);  // 확장자 추출
+      const basename = path.basename(file.originalname, ext);
+      done(null, basename + '_' + new Date().getTime + ext);
+    }
+  }),
+  limits: { fileSize: 20 * 1024 * 1024 },  // 20mb
+});
 
+// 게시물 업로드
+router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {  // POST /post
+  try {
+    const hashtags = req.body.content.match(/#[^\s#]+/g);
+    const post = await Post.create({
+      content: req.body.content,
+      UserId: req.user.id,
+    });
+    if (hashtags) {
+      const result = await Promise.all(hashtags.map((tag) => Hashtag.findOrCreate({ // 있으면 가져오고 없으면 등록
+        where: { name: tag.slice(1).toLowerCase() },
+      })));
+      await post.addHashtags(result.map((v) => v[0]));
+    }
+    if (req.body.image) {
+      if (Array.isArray(req.body.image)) {  // 이미지를 여러개 올리면 image: [image1.png, image2.png]
+        const images = await Promise.all(req.body.image.map((image) => Image.create({ src: image })));
+        await post.addImages(images);
+      } else {  // 이미지를 하나만 올리면 image: image.png
+        const image = await Image.create({ src: req.body.image });
+        await post.addImages(image);
+      }
+    }
+    const fullPost = await Post.findOne({
+      where: { id: post.id },
+      include: [{
+        model: Image,
+      }, {
+        model: Comment,
+        include: [{
+          model: User, // 댓글 작성자
+          attributes: ['id', 'username'],
+        }],
+      }, {
+        model: User, // 게시글 작성자
+        attributes: ['id', 'username'],
+      }, {
+        model: User, // 좋아요 누른 사람
+        as: 'Likers',
+        attributes: ['id'],
+      }]
+    })
+    res.status(201).json(fullPost);
   } catch (error) {
     console.error(error);
     next(error);
   }
+});
+// 이미지 업로드
+router.post('/images', isLoggedIn, upload.array('image'), (req, res, next) => { // POST /post/images
+  res.json(req.files.map((v) => v.filename));
 });
 
 module.exports = router;
