e125bf0dd68365cdf8e8b0f79ba0ffc4f65bcfee
gipark <gipark1993@gmail.com>
2020-08-19 23:51:03 +0900

    add footer

--- front/components/Footer.js
+++ front/components/Footer.js
@@ -2,12 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 const FooterWrapper = styled.div`
-  height: 104px;
+  max-width: 935px;
+  width: 100%;
+  height: 28px;
+  margin: 38px auto;
 `;
-const Footer = () => {
-  return (
-    <FooterWrapper>footer</FooterWrapper>
-  );
-};
+const Footer = () => (
+  <FooterWrapper>
+    <a
+      style={{ float: 'right' }}
+      href="https://github.com/GihongPark"
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      Made by Gipark
+    </a>
+  </FooterWrapper>
+);
 
 export default Footer;
