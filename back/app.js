563e79c5244a3c11f269f92138df8517619397c6
gipark <gipark1993@gmail.com>
2020-08-21 17:42:10 +0900

    update cookie sharing

--- back/app.js
+++ back/app.js
@@ -54,6 +54,11 @@ app.use(session({
   saveUninitialized: false,
   resave: false,
   secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+    domain: process.env.NODE_ENV === 'production' && '.hongsta.com'
+  },
 }));
 app.use(passport.initialize());
 app.use(passport.session());
