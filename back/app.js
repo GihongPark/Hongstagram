c4e2787b71c17ae4e93b43971c8b3dc0d33cde19
gipark <gipark1993@gmail.com>
2020-08-21 15:12:16 +0900

    update aws  ip change

--- back/app.js
+++ back/app.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(morgan('dev'));
 }
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://13.125.142.25', ],
+  origin: ['http://localhost:3000', 'http://54.180.152.221', ],
   credentials: true,  // 쿠키 설정
 }));
 
