a2f8f2a688c1d1a435d4073524dc02e6e6fa0087
gipark <gipark1993@gmail.com>
2020-08-22 16:14:02 +0900

    update lambda 오타

--- lambda/index.js
+++ lambda/index.js
@@ -4,16 +4,20 @@ const sharp = require('sharp');
 const s3 = new AWS.S3();
 
 exports.handler = async (event, context, callback) => {
-  const Bucket = event.Records[0].s3.bucket.name;
-  const Key = event.Records[0].s3.object.key;
+  const Bucket = event.Records[0].s3.bucket.name; // react-nodebird-s3
+  const Key = event.Records[0].s3.object.key; // original/12312312_abc.png
+  console.log(Bucket, Key);
   const filename = Key.split('/')[Key.split('/').length - 1];
-  const ext = Key.spit('.')[Key.split('.').length - 1];
+  const ext = Key.split('.')[Key.split('.').length - 1];
   const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
+  console.log('filename', filename, 'ext', ext);
+
 
   try {
     const s3Object = await s3.getObject({ Bucket, Key }).promise();
+    console.log('original', s3Object.Body.length);
     const resizedImage = await sharp(s3Object.Body)
-      .resize(320, 320, { fit: 'inside' })
+      .resize(400, 400, { fit: 'inside' })
       .toFormat(requiredFormat)
       .toBuffer();
     await s3.putObject({
@@ -21,6 +25,7 @@ exports.handler = async (event, context, callback) => {
       Key: `thumb/${filename}`,
       Body: resizedImage,
     }).promise();
+    console.log('put', resizedImage.length);
     return callback(null, `thumb/${filename}`);
   } catch (error) {
     console.error(error);
