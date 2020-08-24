9c6e5098e875c334f2aa7ce3526d86c48a06238a
gipark <gipark1993@gmail.com>
2020-08-25 00:23:18 +0900

    update list width

--- front/pages/index.js
+++ front/pages/index.js
@@ -19,7 +19,7 @@ const Home = () => {
   const {
     mainPosts, hasMorePosts, loadPostsLoading, loadPostsDone,
   } = useSelector((state) => state.post);
-  const style = useMemo(() => ({ maxWidth: '614px', width: '90%' }));
+  const style = useMemo(() => ({ maxWidth: '600px', width: '90%' }));
 
   useEffect(() => {
     if (!me || !me.id) {
